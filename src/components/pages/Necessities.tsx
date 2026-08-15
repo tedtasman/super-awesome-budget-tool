6c5781c4bf487d4234a208196498e40e502779d8
Ted Tasman <tktasman@gmail.com>
2026-08-14 23:46:24 -0400

    Add amortization range selector

--- src/components/pages/Necessities.tsx
+++ src/components/pages/Necessities.tsx
@@ -1,20 +1,24 @@
 import { useNecessitiesStore } from "../../store/ExpensesStore";
 import { useState } from "react";
 import ExpenseTable from "../ui/ExpenseTable";
+import TimeframeSelector from "../ui/TimeframeSelector";
 
 export default function Necessities() {
   const necessities = useNecessitiesStore((state) => state.expenses);
   const addNecessity = useNecessitiesStore((state) => state.addExpense);
 
   const [timeframe, setTimeFrame] = useState<
-    "week" | "month" | "year" | "paycheck"
+    "week" | "month" | "year" | "paycheck" | number
   >("month");
 
   return (
-    <ExpenseTable
-      timeframe={timeframe}
-      addExpense={addNecessity}
-      expenses={necessities}
-    />
+    <>
+      <TimeframeSelector timeframe={timeframe} setTimeFrame={setTimeFrame} />
+      <ExpenseTable
+        timeframe={timeframe}
+        addExpense={addNecessity}
+        expenses={necessities}
+      />
+    </>
   );
 }
