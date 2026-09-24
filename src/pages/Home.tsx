2a0236632142d035c3ed21afb125342efdfc5d7b
Ted Tasman <tktasman@gmail.com>
2026-09-23 22:19:57 -0400

    update homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,11 @@ import PageCore from "../ui/PageCore";
 export default function Home() {
   return (
     <PageCore pageTitle="Home" className="home">
-      <h5>Welcome to Super Awesome Budget Tool</h5>
-      <p>This is, surprisingly, a work in progress.</p>
+      <h5>Welcome to Super Awesome Budget Tool.</h5>
+      <small>Or probably something else. Name is work in progress. So is everything else.</small>
+      <p>
+        A <strong>tool</strong> used to turn a financial picture into a budget.
+      </p>
     </PageCore>
   );
 }
